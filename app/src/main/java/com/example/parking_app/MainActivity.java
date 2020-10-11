d7d7d13e0ddf2b2cf5f49817c8edf9e2b93d3809
moocluck <mymoodisduck@gmail.com>
2020-10-11 11:14:12 +0300

    Version 1.0.1

--- app/src/main/java/com/example/parking_app/MainActivity.java
+++ app/src/main/java/com/example/parking_app/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.parking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startBtn);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent (MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/parking_app/SecondActivity.java
+++ app/src/main/java/com/example/parking_app/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.parking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button qrButton;
+    EditText editTextTextPersonName, editTextTextPersonName2, editTextTextPersonName3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        qrButton = findViewById(R.id.qrBtn);
+
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int fName = (editTextTextPersonName.getText() == null)?0:Integer.parseInt(editTextTextPersonName.getText().toString());
+                int lName = (editTextTextPersonName2.getText() == null)?0:Integer.parseInt(editTextTextPersonName2.getText().toString());
+                int carNumber = (editTextTextPersonName3.getText() == null)?0:Integer.parseInt(editTextTextPersonName3.getText().toString());
+
+                Intent intent = new Intent (SecondActivity.this, Qr_scanner.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
